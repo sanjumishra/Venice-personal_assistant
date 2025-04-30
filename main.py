05b3fb66c644d0e9420a66ed9b37b8cfcbcfbb3d
Sanju Mishra <contact.sanjumishra@gmail.com>
2025-04-30 20:19:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import eel
+
+from engine.features import *
+from engine.command import *
+
+eel.init('front-end')
+
+playAssistantSound()
+
+os.system('start chrome.exe --app="http://localhost:8000/index.html"')
+eel.start('index.html', mode=None, host='localhost', block=True)
